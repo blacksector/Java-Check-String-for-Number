a0d4f307162bb1f32c826d4b007015caf529bd28
Omar Quazi <omarquaz@gmail.com>
2015-10-01 23:59:54 -0400

    Create ReadyToProgram.java
    This is source code written in "Ready to Program".

---
+++ ReadyToProgram.java
@@ -0,0 +1,31 @@
+// This is a "Ready to Program" example:
+
+import hsa.Console;
+
+public class ReadyToProgram extends StringtoNumber {
+
+
+    public static void main(String args[]) {
+        
+        Console c = new Console();
+        c.println("*NOTE: Please enter 'exit' to exit program.*\n\n");
+
+        while (true) {
+            c.println("Please enter a number:");
+             String theInput = c.readLine();
+             String a = containsNum(theInput);
+             if (theInput == "exit") {
+                 break;
+             }
+             if (a == "1") {
+                 c.println("That is a number! :D");
+             } else {
+                 c.println("That has characters!");
+             }
+        
+    }
+
+
+
+
+}
